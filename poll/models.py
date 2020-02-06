868ec6ef12bb474c99f958e595ff6bb139ee2935
almabud <almabud37@gmail.com>
2020-02-06 22:25:21 +0600

    test file added

--- poll/models.py
+++ poll/models.py
@@ -11,10 +11,10 @@ class Question(models.Model):
     pub_date = models.DateTimeField('published date')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
 
     def __str__(self):
-        return self
+        return self.question_text
 
 
 class Choice(models.Model):
@@ -23,7 +23,7 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self
+        return self.choice_text
 
 
 
--- poll/views.py
+++ poll/views.py
@@ -1,8 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
+from django.template import loader
+from django.template.defaulttags import csrf_token
+from django.urls import reverse
+from django.views import generic
+
+from poll.models import Question, Choice
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    template_name = 'poll/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'poll/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'poll/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'poll/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('poll:result', args=(question.id,)))
